997477199c2460615264efc1ca0b9c03cb8d9686
Brunno Quadrado <bbertoninuti@gmail.com>
2024-08-17 15:55:11 -0300

    starting to implement Board and piece.

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Program {
@@ -8,6 +9,9 @@ public class Program {
 		
 		Position position1 = new Position(3,5);
 		System.out.println(position1);
+		
+		Board board = new Board(8,8);
+		
 	}
 
 }
