bd499e65742a760ed509623df46d8826e4f755e6
Brunno Quadrado <bbertoninuti@gmail.com>
2024-08-18 17:52:38 -0300

    ChessException and ChessPosition.

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -8,27 +8,32 @@ import chess.pieces.Rook;
 public class ChessMatch {
 
 	private Board board;
-	
+
 	public ChessMatch() {
-		board = new Board(8,8);
+		board = new Board(8, 8);
 		initialSetup();
 	}
-	
+
 	public ChessPiece[][] getPieces() {
 		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
 		for (int i = 0; i < board.getRows(); i++) {
-			for(int j = 0; j < board.getColumns(); j ++) {
-				mat[i][j] = (ChessPiece) board.piece(i, j);		
+			for (int j = 0; j < board.getColumns(); j++) {
+				mat[i][j] = (ChessPiece) board.piece(i, j);
 			}
 		}
 		return mat;
 	}
-	
-	private void initialSetup() {
-		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
-		board.placePiece(new King(board, Color.BLACK), new Position(7,4));
-		board.placePiece(new King(board, Color.BLACK), new Position(7,4));
+
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
 	}
-	
-	
+
+	private void initialSetup() {
+		for (char test = 'a'; test <= 'h'; test++) {
+			placeNewPiece(test, 2, new Rook(board, Color.WHITE));
+			placeNewPiece(test, 7, new Rook(board, Color.BLACK));
+		}
+		
+	}
+
 }
