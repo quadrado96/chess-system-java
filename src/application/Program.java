673199959dc9ca025f4c6442fdd0da4cf88f5c0c
Brunno Quadrado <bbertoninuti@gmail.com>
2024-08-19 10:38:24 -0300

    handling exceptions and clearing screen.

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Scanner;
 
+import boardgame.BoardException;
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
@@ -9,22 +11,31 @@ import chess.ChessPosition;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		ChessMatch chessMatch = new ChessMatch();
 		while (true) {
-			UI.printBoard(chessMatch.getPieces());
-			System.out.println("\n\nSource: ");
-			ChessPosition source = UI.readChessPosition(sc);
-			
-			System.out.println("\nTarget: ");
-			ChessPosition target = UI.readChessPosition(sc);
-			
-			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			try {
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces());
+				System.out.println("\n\nSource: ");
+				ChessPosition source = UI.readChessPosition(sc);
+
+				System.out.println("\nTarget: ");
+				ChessPosition target = UI.readChessPosition(sc);
+
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+
+			} catch (ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			} catch (BoardException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
 		}
-		
-		
+
 	}
 
 }
